fc8a621bbc8c677657b6d0dd45dba776b88ffed4
DESKTOP-CE4PUCU\jiangbo <teddy99999@sina.cn>
2019-06-23 21:44:21 +0800

    不指定tag的节点(Default)必须是第一个节点

--- ConfMgmt/TestConfMgmt/TestXmlBuild.cs
+++ ConfMgmt/TestConfMgmt/TestXmlBuild.cs
@@ -115,6 +115,16 @@ namespace TestConfMgmt
             JbAssert.Equal(conf[@"T1.2&T1.2.2:Item2"], "1.2.2.2");
         }
         [TestMethod]
+        public void TestXmlBuild_MultiTagWithDefault()
+        {
+            ConfTree conf = XmlBuilder.Generate($@"{GlobalVariables.SamplePath}/MultiTagWithDefault.xml");
+            Debug.WriteLine(conf.ToString());
+
+            JbAssert.Equal(conf[@"T1.1:Item1"], "1.1.1.1");
+            JbAssert.Equal(conf[@"T1.2:Item1"], "1.2.2.1");
+            JbAssert.Equal(conf[@"T1.2.1:Item1"], "1.2.1.3");
+        }
+        [TestMethod]
         public void TestXmlBuild_MultiLevel()
         {
             ConfTree conf = XmlBuilder.Generate($@"{GlobalVariables.SamplePath}/MultiLevel.xml");
