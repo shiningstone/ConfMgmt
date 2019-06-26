acac1c1b3ff9d56a795bfd281dbd92fab7386c40
DESKTOP-CE4PUCU\jiangbo <teddy99999@sina.cn>
2019-06-26 21:14:01 +0800

    添加DictionaryBuilder

---
+++ ConfMgmt/ConfMgmt/DictionaryBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace JbConf
+{
+    public class DictionaryBuilder
+    {
+        public static ConfTree Generate(Dictionary<string, string> kvs, string name)
+        {
+            ConfTree result = new ConfTree(name);
+            result.Path = "";
+            result.Source = Source.Dictionary;
+            result.Tag = "Default";
+
+            foreach (var kv in kvs)
+            {
+                result.Add(new ConfItem(kv.Key, kv.Value));
+            }
+            ConfMgmt.Add(result);
+            return result;
+        }
+    }
+}
