66c6775edc07243018d332f6060144e1b49b1598
DESKTOP-CE4PUCU\jiangbo <teddy99999@sina.cn>
2020-05-11 13:48:32 +0800

    自动编版本

--- ConfMgmt/ConfView/Properties/AssemblyInfo.cs
+++ ConfMgmt/ConfView/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("85002cd1-e861-4fb6-a9cc-1a60876025a8")]
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-// 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
-//通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
